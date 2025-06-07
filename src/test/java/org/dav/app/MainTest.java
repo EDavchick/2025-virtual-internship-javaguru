98cc20046e04dc0cd949601a215c58737f137f24
EDavchick <eledavchik@gmail.com>
2025-06-07 19:10:43 +0400

    gitignore

--- src/test/java/org/dav/app/MainTest.java
+++ src/test/java/org/dav/app/MainTest.java
@@ -3,11 +3,13 @@ package org.dav.app;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class MainTest {
 
     @Test
     public void test(){
-
+        assertTrue(true);
     }
 
 }
